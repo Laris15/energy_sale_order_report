818b8054752a41555fe8877c9955419701a0586e
larissa <simolarissa.com>
2023-07-29 05:17:26 +0200

    créer le champ operation_type dans le model sale_order pour pouvoir recupérer sa valeur et l%afficher dans le rapport

--- models/sale_order.py
+++ models/sale_order.py
@@ -6,6 +6,9 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    operation_type = fields.Selection(
+        [('BAT_146', 'BAT TH 146'), ('BAR_160', 'BAR TH  160'), ('BAT_103', 'BAT-EN-103')],
+        string="Type d'operation", required=True)
     def get_total_qty(self):
         total_qty = sum(self.order_line.mapped('product_uom_qty'))
         return total_qty
